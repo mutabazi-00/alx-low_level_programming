7de741bbbd2bb87a6fe25961a3bf6dfe5377034d
mutabazi-00 <mutajean30@gmail.com>
2022-06-14 12:26:10 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ *
+ * @str: A pointer to an int that will be changed
+ *
+ * Return: void wich means the answer is correct.
+ */
+
+void _puts(char *str)
+{
+	char *c;
+	int m;
+
+	c = str;
+
+	for (m = 0; c[m]; m++)
+	{
+		_putchar (c[m]);
+	}
+	_putchar('\n');
+}
