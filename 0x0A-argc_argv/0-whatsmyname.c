2b9d8adeb805a02d9ddb6c77c2454c07d87dba31
mutabazi-00 <mutajean30@gmail.com>
2022-06-24 11:50:32 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prototype that help print program's name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Program's name
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
